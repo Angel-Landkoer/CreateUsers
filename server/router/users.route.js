3d2433ec6c44670e498091249d67b0663ed67c0b
angell <angel.landkoer09@gmail.com>
2024-05-25 10:55:54 -0500

    Aplicando validador de datos

--- server/router/users.route.js
+++ server/router/users.route.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { createUser, deleteUser, getUserId, getUsers } from '../controllers/users.controller.js'
+import { validatorHandler } from '../middlewares/validator.handler.js'
+import { createUserSch, idUser } from '../schemas/users.schema.js'
 
 const route = express.Router()
 
 route.get("/", getUsers)
 
-route.get("/:id", getUserId)
+route.get("/:id", validatorHandler(idUser, "params"), getUserId)
 
-route.post("/", createUser)
+route.post("/", validatorHandler(createUserSch, "body"), createUser)
 
-route.delete("/:id", deleteUser)
+route.delete("/:id", validatorHandler(idUser, "params"), deleteUser)
 
 export default route
