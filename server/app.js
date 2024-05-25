1e61de6c67418fa9421d798d496757853d4fba6e
angell <angel.landkoer09@gmail.com>
2024-05-25 11:01:33 -0500

    Agregando middleware

--- server/app.js
+++ server/app.js
@@ -1,20 +1,21 @@
-import express from 'express'
+import express, { json } from 'express'
 import { routeAPI } from './router/index.js'
 import { boomErrorHandler, errorHandler } from './middlewares/error.handler.js'
 
 export function app(PORT) {
   const app = express()
-
+  
   // Delete name express
   app.disable("x-powered-by")
-
+  
   app.get("/", (req, res) => { res.send("<h1>Welcome</h1>") })
-
-
+  
+  
   // Routes
   routeAPI(app)
-
+  
   // Middlewares
+  app.use(json())
   app.use(boomErrorHandler)
   app.use(errorHandler)
 
