944f70e2855b56c2cd6dbaddd0700834c1a210ce
angell <angel.landkoer09@gmail.com>
2024-05-25 11:17:52 -0500

    Importando y aplicando Cors

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
 import express, { json } from 'express'
 import { routeAPI } from './router/index.js'
 import { boomErrorHandler, errorHandler } from './middlewares/error.handler.js'
+import { corsHandler } from './middlewares/cors.handler.js'
 
 export function app(PORT) {
   const app = express()
-  
+
   // Delete name express
   app.disable("x-powered-by")
-  
+
   app.get("/", (req, res) => { res.send("<h1>Welcome</h1>") })
-  
-  
+
+
   // Routes
   routeAPI(app)
-  
+
   // Middlewares
   app.use(json())
+  app.use(corsHandler())
   app.use(boomErrorHandler)
   app.use(errorHandler)
 
