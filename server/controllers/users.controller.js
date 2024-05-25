da4f3fc79f6918baddc32381f700778bac7b6a7b
angell <angel.landkoer09@gmail.com>
2024-05-25 10:53:28 -0500

    Aplicando la logica de negocio

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,18 +1,23 @@
+import { UserService } from '../models/sqlite/users.service.js'
 
+const service = UserService
 
 export const getUsers = async (req, res, next) => {
   try {
+    const data = await service.getAll()
 
-    res.status(200).json({})
+    res.status(200).json({ data: data.rows })
   } catch (err) {
     next(err)
   }
 }
 
 export const getUserId = async (req, res, next) => {
-  try {
 
-    res.status(200).json({})
+  const { id } = req.params
+  try {
+    const data = await service.getId(id)
+    res.status(200).json({ data: data.rows })
   } catch (err) {
     next(err)
   }
@@ -20,9 +25,11 @@ export const getUserId = async (req, res, next) => {
 
 
 export const createUser = async (req, res, next) => {
-  try {
 
-    res.status(201).json({})
+  const body = req.body
+  try {
+    const data = await service.create(body)
+    res.status(201).json({ data })
   } catch (err) {
     next(err)
   }
@@ -30,9 +37,12 @@ export const createUser = async (req, res, next) => {
 
 
 export const deleteUser = async (req, res, next) => {
-  try {
 
-    res.status(204).json({})
+  const { id } = req.params
+  try {
+    const data = await service.delete(id)
+
+    res.status(204).json({ data })
   } catch (err) {
     next(err)
   }
